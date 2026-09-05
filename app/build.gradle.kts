829709106b9b9a1ebf5866b18ce068b5e4adee12
sedonet23 <sedonet23@gmail.com>
2026-09-05 15:49:04 +0300

    build: target stable Android 16 API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.okulyonetim.optikokuyucu"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.okulyonetim.optikokuyucu"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
 
