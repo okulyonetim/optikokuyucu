7a54cc7e05df861f2514fe1bbe7a996b84ba8f57
sedonet23 <sedonet23@gmail.com>
2026-09-05 19:06:03 +0300

    chore: release student identity and serial scanning build v0.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.okulyonetim.optikokuyucu"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.7.0"
+        versionCode = 9
+        versionName = "0.8.0"
 
         vectorDrawables {
             useSupportLibrary = true
