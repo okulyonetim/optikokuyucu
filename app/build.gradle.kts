94f5580b082618d248c908646ac8d2d5be92bca0
sedonet23 <sedonet23@gmail.com>
2026-09-05 18:48:49 +0300

    chore: release live and 100-question benchmark build v0.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.okulyonetim.optikokuyucu"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.6.0"
+        versionCode = 8
+        versionName = "0.7.0"
 
         vectorDrawables {
             useSupportLibrary = true
