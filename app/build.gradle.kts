68cb257b89134f662904cc9274f733a3a2f28405
sedonet23 <sedonet23@gmail.com>
2026-09-05 18:09:43 +0300

    chore: release glare robustness fix v0.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.okulyonetim.optikokuyucu"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.5.0"
+        versionCode = 6
+        versionName = "0.5.1"
 
         vectorDrawables {
             useSupportLibrary = true
