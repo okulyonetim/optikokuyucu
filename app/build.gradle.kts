896c6112c44a9f12a5b547e7e818c46bc540ab3f
sedonet23 <sedonet23@gmail.com>
2026-09-05 20:08:51 +0300

    chore: release visual designer safety milestone v0.12.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.okulyonetim.optikokuyucu"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "0.11.0"
+        versionCode = 13
+        versionName = "0.12.0"
 
         vectorDrawables {
             useSupportLibrary = true
