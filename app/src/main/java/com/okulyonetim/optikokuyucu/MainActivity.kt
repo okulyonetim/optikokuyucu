f2143029f55039c891c30e5aec51bdd07f10d839
sedonet23 <sedonet23@gmail.com>
2026-09-05 16:13:53 +0300

    feat: initialize OpenCV at app startup

--- app/src/main/java/com/okulyonetim/optikokuyucu/MainActivity.kt
+++ app/src/main/java/com/okulyonetim/optikokuyucu/MainActivity.kt
@@ -5,13 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.okulyonetim.optikokuyucu.ui.OmrCameraScreen
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val openCvReady = OpenCVLoader.initLocal()
+
         setContent {
-            OmrCameraScreen()
+            OmrCameraScreen(openCvReady = openCvReady)
         }
     }
 }
