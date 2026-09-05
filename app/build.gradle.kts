dc272a544734820d3db38c685a1cdc8d56ab1b0b
sedonet23 <sedonet23@gmail.com>
2026-09-05 17:44:57 +0300

    chore: release advanced OMR stress build v0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.okulyonetim.optikokuyucu"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
 
         vectorDrawables {
             useSupportLibrary = true
