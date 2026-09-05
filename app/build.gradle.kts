9cca7e04d6e09b9a1954d0351df65382c04e0359
sedonet23 <sedonet23@gmail.com>
2026-09-05 19:46:24 +0300

    chore: release designer PDF and drag milestone v0.10.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.okulyonetim.optikokuyucu"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.9.0"
+        versionCode = 11
+        versionName = "0.10.0"
 
         vectorDrawables {
             useSupportLibrary = true
