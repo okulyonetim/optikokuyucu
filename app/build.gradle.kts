e61af9e2e96359a1d2a54ae7f4ada0713a7b8147
sedonet23 <sedonet23@gmail.com>
2026-09-06 21:30:52 +0300

    build: bump fixed physical scale version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,8 @@ android {
         applicationId = "com.okulyonetim.optikokuyucu"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "0.12.6"
+        versionCode = 20
+        versionName = "0.12.7"
 
         vectorDrawables {
             useSupportLibrary = true
