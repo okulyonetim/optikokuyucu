0d59acec88f8e03c9a4d4ef5bd374d806f5d0a6f
sedonet23 <sedonet23@gmail.com>
2026-09-07 00:31:49 +0300

    chore: bump app version to 0.14.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,8 @@ android {
         applicationId = "com.okulyonetim.optikokuyucu"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "0.13.1"
+        versionCode = 25
+        versionName = "0.14.0"
 
         vectorDrawables {
             useSupportLibrary = true
