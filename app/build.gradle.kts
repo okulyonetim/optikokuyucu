5d63c392e8c4b207932fabfb9fe5636b2edc8812
sedonet23 <sedonet23@gmail.com>
2026-09-07 06:11:25 +0300

    chore: bump app to 0.15.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,8 @@ android {
         applicationId = "com.okulyonetim.optikokuyucu"
         minSdk = 26
         targetSdk = 36
-        versionCode = 32
-        versionName = "0.15.0"
+        versionCode = 33
+        versionName = "0.15.1"
 
         vectorDrawables {
             useSupportLibrary = true
