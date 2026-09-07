9b3d069fbc7e06d2a1dc25a76187426252dae929
sedonet23 <sedonet23@gmail.com>
2026-09-07 15:30:03 +0300

    chore: bump regression fix to 0.15.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
         applicationId = "com.okulyonetim.optikokuyucu"
         minSdk = 26
         targetSdk = 36
-        versionCode = 42
-        versionName = "0.15.10"
+        versionCode = 43
+        versionName = "0.15.11"
         vectorDrawables { useSupportLibrary = true }
     }
     buildTypes {
