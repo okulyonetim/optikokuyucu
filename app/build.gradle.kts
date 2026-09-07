f09c2e9db0ec8a3bac3e33ee8cb9204150190033
sedonet23 <sedonet23@gmail.com>
2026-09-07 23:22:43 +0300

    chore: bump version to 0.18.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
         applicationId = "com.okulyonetim.optikokuyucu"
         minSdk = 26
         targetSdk = 36
-        versionCode = 52
-        versionName = "0.18.0"
+        versionCode = 53
+        versionName = "0.18.1"
         vectorDrawables { useSupportLibrary = true }
     }
     buildTypes {
