840eb1b93ff6279b243d6f0f6d8309908f57f252
sedonet23 <sedonet23@gmail.com>
2026-09-07 10:43:17 +0300

    chore: bump version to 0.15.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,8 @@ android {
         applicationId = "com.okulyonetim.optikokuyucu"
         minSdk = 26
         targetSdk = 36
-        versionCode = 37
-        versionName = "0.15.5"
+        versionCode = 38
+        versionName = "0.15.6"
 
         vectorDrawables {
             useSupportLibrary = true
@@ -91,7 +91,7 @@ android {
 
     packaging {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "/META-INF/{AL2.0,LGPL2.1)"
         }
     }
 }
