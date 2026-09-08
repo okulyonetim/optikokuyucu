30e6517ec5dbabd9517361952e4ee05857a4f640
sedonet23 <sedonet23@gmail.com>
2026-09-08 05:58:16 +0300

    chore: bump app version for brand theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
         applicationId = "com.okulyonetim.optikokuyucu"
         minSdk = 26
         targetSdk = 36
-        versionCode = 57
-        versionName = "0.19.1"
+        versionCode = 58
+        versionName = "0.19.2"
         vectorDrawables { useSupportLibrary = true }
     }
     buildTypes {
