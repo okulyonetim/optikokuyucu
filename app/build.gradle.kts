bb1619a1674ebbfb0ed3e2d73388a0acb55206d7
sedonet23 <sedonet23@gmail.com>
2026-09-08 17:26:21 +0300

    chore: bump version for answer key redesign

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
         applicationId = "com.okulyonetim.optikokuyucu"
         minSdk = 26
         targetSdk = 36
-        versionCode = 70
-        versionName = "0.19.14"
+        versionCode = 71
+        versionName = "0.19.15"
         vectorDrawables { useSupportLibrary = true }
     }
     buildTypes {
