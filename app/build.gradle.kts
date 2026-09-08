546eeaf9bd7dcd72885013c233d27ef7b11c8e8d
sedonet23 <sedonet23@gmail.com>
2026-09-08 08:15:36 +0300

    chore: bump version for report redesign

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
         applicationId = "com.okulyonetim.optikokuyucu"
         minSdk = 26
         targetSdk = 36
-        versionCode = 63
-        versionName = "0.19.7"
+        versionCode = 64
+        versionName = "0.19.8"
         vectorDrawables { useSupportLibrary = true }
     }
     buildTypes {
