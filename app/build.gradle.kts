a2d3ff89564ae1952c0f6d0a988bd3c82170ade4
sedonet23 <sedonet23@gmail.com>
2026-09-08 08:26:35 +0300

    chore: bump version for editor workspace redesign

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
         applicationId = "com.okulyonetim.optikokuyucu"
         minSdk = 26
         targetSdk = 36
-        versionCode = 64
-        versionName = "0.19.8"
+        versionCode = 65
+        versionName = "0.19.9"
         vectorDrawables { useSupportLibrary = true }
     }
     buildTypes {
