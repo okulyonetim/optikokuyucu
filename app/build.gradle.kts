fd338230bb87c42722875cc648e85d469f3540dd
sedonet23 <sedonet23@gmail.com>
2026-09-08 09:32:49 +0300

    chore: bump version for new exam redesign

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
         applicationId = "com.okulyonetim.optikokuyucu"
         minSdk = 26
         targetSdk = 36
-        versionCode = 68
-        versionName = "0.19.12"
+        versionCode = 69
+        versionName = "0.19.13"
         vectorDrawables { useSupportLibrary = true }
     }
     buildTypes {
