6fd37e01c606637e28d50bcd4998d5d63b782733
sedonet23 <sedonet23@gmail.com>
2026-09-10 22:18:16 +0300

    feat: expose combined student identity field

--- app/src/main/java/com/okulyonetim/optikokuyucu/omr/designer/DesignerAreaCatalog.kt
+++ app/src/main/java/com/okulyonetim/optikokuyucu/omr/designer/DesignerAreaCatalog.kt
@@ -9,6 +9,7 @@ enum class DesignerAreaKind(val displayName: String) {
     NUMBER("Numara"),
     ANSWERS("Cevaplar"),
     BOOKLET("Kitapçık Türü"),
+    STUDENT_IDENTITY_LINE("Ad Soyad • Numara • Sınıf"),
     STUDENT_NAME("Öğrenci Adı Soyadı"),
     STUDENT_CLASS("Sınıfı"),
     STUDENT_NUMBER_TEXT("Öğrenci Numarası"),
@@ -31,6 +32,7 @@ object DesignerAreaCatalog {
         DesignerAreaSection(
             "Öğrenci / Sınav Bilgileri",
             listOf(
+                DesignerAreaKind.STUDENT_IDENTITY_LINE,
                 DesignerAreaKind.STUDENT_NAME,
                 DesignerAreaKind.STUDENT_CLASS,
                 DesignerAreaKind.STUDENT_NUMBER_TEXT,
@@ -105,6 +107,12 @@ object DesignerAreaCatalog {
             labelAlignment = DesignerTextAlignment.CENTER
         )
     }
+
+    fun createStudentIdentityLineArea(document: DesignerDocument): DesignerTextElement =
+        DesignerTextPresets.studentIdentityLine(
+            document,
+            createPresetTextArea(document, "student-identity-line", "Öğrenci Bilgileri")
+        )
 
     fun createStudentNameArea(document: DesignerDocument): DesignerTextElement =
         createPresetTextArea(document, "student-name", "Öğrenci Adı Soyadı")
